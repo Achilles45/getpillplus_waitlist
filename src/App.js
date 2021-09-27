3e84203dbe06968c01678293461d61d3b87efe83
Achilles45 <achillesusuoyibo7@gmail.com>
2021-09-27 13:29:15 -0700

    Initial file cleanups

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Getpillplus</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Join our waitlist and we will notify immediately we finally launch. Built with ❤ at Getpillplus Lab
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
