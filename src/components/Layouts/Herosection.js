17995768126b72d63897a0f66fd6be638908616a
Achilles45 <achillesusuoyibo7@gmail.com>
2021-09-30 15:36:25 -0700

    Initial waitlist page designs

---
+++ src/components/Layouts/Herosection.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import './Herosection.scss';
+import woman from '../../images/woman-image.png'
+import roundIcon from '../../images/round-shape.svg'
+import pattern from '../../images/pattern.svg'
+
+export default class Herosection extends Component {
+    render() {
+        return (
+            <div className="herosection">
+                <div className="herosection__icons__holder d-none d-lg-block">
+                    <img src={ roundIcon } className="icons round-icon" alt="" />
+                    <img src={ pattern } className="icons pattern1" alt="" />
+                    <img src={ pattern } className="icons pattern2" alt="" />
+                    <img src={ pattern } className="icons pattern3" alt="" />
+                </div>
+                <div className="balls__holder d-none d-lg-block">
+                    <div className="ball ball1"></div>
+                    <div className="ball ball2"></div>
+                    <div className="ball ball3"></div>
+                    <div className="ball ball4"></div>
+                    <div className="ball ball5"></div>
+                </div>
+               <div className="container">
+                <div className="row">
+                        <div className="col-md-5 herosection__content">
+                            <h1>Redefining access to quality health care</h1>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum assumenda numquam aliquid fugiat qui aut asperiores atque modi, labore quod!</p>
+                            <form className="waitlist__form d-flex justify-content-betweeb">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Enter your email address" />
+                                <button
+                                    type="submit" 
+                                    className="submit__btn">Join Our Waitlist</button>
+                            </form>
+                        </div>
+                        <div className="col-md-1"></div>
+                        <div className="col-md-6 d-none d-lg-block">
+                            <img src={ woman } className="img-fluid" alt="Woman" />
+                        </div>
+                    </div>
+               </div>
+            </div>
+        )
+    }
+}
---
+++ src/components/Layouts/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.scss'
+import logo from '../../images/logo.png';
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="navbar__content d-flex justify-content-between">
+                    <div className="navbar__left">
+                        <img src={ logo } className="logo" alt="PillPlus Logo" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
