8d0b266bb8ea356b7523fe3f3e357d3cc9d7d9d1
Achilles45 <achillesusuoyibo7@gmail.com>
2021-10-02 15:36:53 -0700

    Chnaged background to white

--- src/components/Layouts/Herosection.js
+++ src/components/Layouts/Herosection.js
@@ -1,36 +1,63 @@
 import React, { Component } from 'react'
 import './Herosection.scss';
 import woman from '../../images/woman-image.png'
-import roundIcon from '../../images/round-shape.svg'
-import pattern from '../../images/pattern.svg'
+// import roundIcon from '../../images/round-shape.svg'
+// import pattern from '../../images/pattern.svg'
+
+
+//Firebase imports
 
 export default class Herosection extends Component {
+        //State
+        state = {
+            email: ""
+        }
+
+        //Methods
+        onChange = (e) => {
+            this.setState({
+                [e.target.name] : e.target.value
+            })
+        }
+
+        //OnForm submit method
+        joinWaitlist = () => {
+            // db.collection('waitlist_users').add({
+            //     email: this.state.email
+            // })
+        }
+
     render() {
+        const { email } = this.state;
         return (
             <div className="herosection">
-                <div className="herosection__icons__holder">
+                {/* <div className="herosection__icons__holder">
                     <img src={ roundIcon } className="icons round-icon" alt="" />
                     <img src={ pattern } className="icons pattern1" alt="" />
                     <img src={ pattern } className="icons pattern2" alt="" />
                     <img src={ pattern } className="icons pattern3" alt="" />
-                </div>
-                <div className="balls__holder">
+                </div> */}
+                {/* <div className="balls__holder">
                     <div className="ball ball1"></div>
                     <div className="ball ball2"></div>
                     <div className="ball ball3"></div>
                     <div className="ball ball4"></div>
                     <div className="ball ball5"></div>
-                </div>
+                </div> */}
                <div className="container">
                 <div className="row">
                         <div className="col-md-5 herosection__content">
                             <h1>Redefining access to quality health care</h1>
                             <p>Get connected to your healthcare provider/patients to receive/offer quality medical services from anywhere in the world</p>
-                            <form className="waitlist__form d-flex justify-content-betweeb">
+                            <form onSubmit={this.joinWaitlist.bind(this)} className="waitlist__form d-flex justify-content-betweeb">
                                 <input
                                     type="text"
                                     className="form-control"
-                                    placeholder="Enter your email address" />
+                                    placeholder="Enter your email address" 
+                                    name="email"
+                                    value={ email }
+                                    onChange={ this.onChange }
+                                    />
                                 <button
                                     type="submit" 
                                     className="submit__btn">Join Our Waitlist</button>
--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.scss'
-import logo from '../../images/pill.png';
+import logo from '../../images/blue-logo.png';
 
 export default function Navbar() {
     return (
